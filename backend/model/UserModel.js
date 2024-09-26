06457eaf234c445b0b73f6eb2dd9c414560f963b
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-26 18:47:30 +0800

    updated student schema

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
   profilePicture: {
     type: String,
   },
@@ -17,21 +20,25 @@ const userSchema = new mongoose.Schema({
 });
 
 // Pre-save hook to hash the password before saving it to the database
-userSchema.pre("save", async function (next) {
-  const user = this;
+userSchema.pre(
+  "save",
+  async function (next) {
+    const user = this;
 
-  // Only hash the password if it has been modified (or is new)
-  if (user.isModified("password")) {
-    try {
-      // Generate a salt and hash the password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-    } catch (err) {
-      return next(err);
+    // Only hash the password if it has been modified (or is new)
+    if (user.isModified("password")) {
+      try {
+        // Generate a salt and hash the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      } catch (err) {
+        return next(err);
+      }
     }
-  }
-  next();
-},{ discriminatorKey: "role", collection: "users" });
+    next();
+  },
+  { discriminatorKey: "role", collection: "users" }
+);
 
 // Method to compare a given password with the hashed password in the database
 userSchema.methods.comparePassword = async function (candidatePassword) {
@@ -41,11 +48,18 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
 // Base UserModel
 const UserModel = mongoose.model("User", userSchema);
 
-
+//Student specific schema
 const StudentSchema = new mongoose.Schema({
+  firstname: { type: String },
+  lastname: { type: String },
+  middlename: { type: String },
+  studentId: { type: String },
+  age: { type: String },
+  contactNumber: { type: String },
+  course: { type: String },
+});
 
-})
-
+//Discriminator for student
 const StudentModel = mongoose.model("2", StudentSchema);
 
-export {UserModel, StudentModel};
+export { UserModel, StudentModel };
