87681882dec0d6e05039ebbbe1e3a751d1b0fd5f
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-27 11:55:45 +0800

    implemented update user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,20 +1,20 @@
-import { UserModel as User } from "../model/UserModel.js";
+import { StudentModel as Student } from "../model/UserModel.js";
 
 export const findUser = async (req, res, next) => {
   //token user id
   const userId = req.user.userId;
   try {
     //verifies if user exists
-    const user = await User.findById(userId).select([
-        "studentId",
-        "firstname",
-        "lastname",
-        "middlename",
-        "age",
-        "email",
-        "contactNumber",
-        "course",
-        "profilePicture"
+    const user = await Student.findById(userId).select([
+      "studentId",
+      "firstname",
+      "lastname",
+      "middlename",
+      "age",
+      "email",
+      "contactNumber",
+      "course",
+      "profilePicture",
     ]);
 
     if (!user) {
@@ -27,6 +27,44 @@ export const findUser = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  const userId = req.user.userId;
+  //ensures that these fields are only updated
+  const { firstname, lastname, middlename, age, contactNumber, course } =
+    req.body;
+  try {
+    //student data
+    const studentData = {
+        firstname, 
+        lastname, 
+        middlename,
+        age, 
+        contactNumber,
+        course
+    }
+    //update student info
+    const user = await Student.findByIdAndUpdate(userId, studentData);
+
+    //verify if user exists
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    //send response message
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
     });
   } catch (error) {
     return res.status(500).json({
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,16 @@
 import express from "express";
 import authenticate, { authorizeRole } from "../middleware/authMiddleware.js";
-import { findUser } from "../controller/userController.js";
+import { findUser, updateUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.get("/", authenticate, authorizeRole("student"), findUser);
-
+router.patch(
+  "/",
+  express.json(),
+  authenticate,
+  authorizeRole("student"),
+  updateUser
+);
 
 export default router;
