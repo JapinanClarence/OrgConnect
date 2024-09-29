0c7e80e56177f85516994514725782cfc831ce61
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-09-30 07:02:03 +0800

    implemented account status validation in create org

--- backend/controller/admin/orgController.js
+++ backend/controller/admin/orgController.js
@@ -6,37 +6,49 @@ export const createOrg = async (req, res, next) => {
 
   const userId = req.user.userId;
   try {
-    const organization = await Organization.findOne({name});
-    //verify if organization name already exists
-    if(organization){
-        return res.status(400).json({
-            success: false,
-            message: "Organization name already exists"
-        })
-    }
     //verify if user exist
     const user = await Admin.findById(userId);
 
-    if(!user){
+    if (!user ) {
       return res.status(404).json({
         success: false,
-        message: "User not found"
-      })
+        message: "User not found",
+      });
+    }
+
+    if(user.active){
+      return res.status(400).json({
+        success: false,
+        message: "Account already created an organization",
+      });
+    }
+
+    const organization = await Organization.findOne({ name });
+    //verify if organization name already exists
+    if (organization) {
+      return res.status(400).json({
+        success: false,
+        message: "Organization name already exists",
+      });
     }
 
     await Organization.create({
-        name, 
-        description,
-        about,
-        contact,
-        user: userId
-    })
+      name,
+      description,
+      about,
+      contact,
+      user: userId,
+    });
+
+    //activate account after creating an org
+    await Admin.findByIdAndUpdate(userId, {
+      active : true
+    });
 
     res.status(201).json({
       success: true,
-      message: "Organization created"
-    })
-
+      message: "Organization created",
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -49,23 +61,25 @@ export const findOrg = async (req, res, next) => {
   const userId = req.user.userId;
 
   try {
-    const org = await Organization.findOne({user: userId}).select("name description about contact banner");
+    const org = await Organization.findOne({ user: userId }).select(
+      "name description about contact banner"
+    );
 
-    if(!org){
+    if (!org) {
       return res.status(404).json({
         success: false,
-        message: "Organization not found"
-      })
+        message: "Organization not found",
+      });
     }
 
     res.status(200).json({
       success: true,
-      data: org
-    })
+      data: org,
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
       message: err.message,
     });
   }
-}
+};
