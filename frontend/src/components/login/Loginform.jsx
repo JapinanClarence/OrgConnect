49893f0ae5a8d769eb95c560e969ba05848880f0
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-04 09:36:04 +0800

    implemented zod for login

--- frontend/src/components/login/Loginform.jsx
+++ frontend/src/components/login/Loginform.jsx
@@ -1,13 +1,45 @@
-import { motion } from "framer-motion";
-import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { LoginSchema } from "@/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoaderCircle } from "lucide-react";
+import { z } from "zod";
 
 const Loginform = () => {
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    const formData = LoginSchema.parse(data);
+
+    console.log(formData);
+  };
   return (
-    <motion.Card
+    <Card
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="m-10 p-8 rounded-xl space-y-4 bg-white max-w-3xl w-[450px] shadow border border-slate-300"
+      className="m-10 rounded-xl space-y-4 bg-white max-w-3xl w-[450px] shadow border border-slate-300"
     >
       <CardHeader>
         <img
@@ -21,12 +53,53 @@ const Loginform = () => {
         </p>
       </CardHeader>
       <CardContent>
-            
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-gray-600 text-sm">
+                      Username
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} type="text" />
+                    </FormControl>
+                    <FormMessage className="text-xs "/>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-gray-600 text-sm">
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} type="text" />
+                    </FormControl>
+                    <FormMessage className="text-xs"/>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div>
+              <Button
+                id="submit"
+                className="bg-gray-900 hover:bg-gray-800 text-md text-white rounded-md px-3 py-2 w-full"
+              >
+                Login
+              </Button>
+            </div>
+          </form>
+        </Form>
       </CardContent>
-      <CardFooter>
-        Sample
-      </CardFooter>
-    </motion.Card>
+      {/* <CardFooter></CardFooter> */}
+    </Card>
   );
 };
 
