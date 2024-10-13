7aa7d460a27dc1e9c1a7b688f36ad8d262b2d6d9
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-13 19:43:27 +0800

    implemented utility function for simple date format

--- frontend/src/util/helpers.js
+++ frontend/src/util/helpers.js
@@ -11,3 +11,15 @@ export const formatDate = (dateString) => {
     hour12: true          // AM/PM format
   });
 };
+
+export const formatSimpleDateTime = (dateString) => {
+  const date = new Date(dateString); // Parse the date string into a Date object
+  return date.toLocaleString("en-US", {
+    year: "numeric",      // Full year
+    month: "2-digit",     // 2-digit month (MM)
+    day: "2-digit",       // Day of the month with leading zero (DD)
+    hour: "numeric",      // Hour in 12-hour format
+    minute: "numeric",    // Minutes
+    hour12: true          // AM/PM format
+  });
+};
