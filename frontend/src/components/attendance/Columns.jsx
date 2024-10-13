ce888535c959c582b91b53109e4e5550f76ea418
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-13 19:43:59 +0800

    implemented get data for attendance

--- frontend/src/components/attendance/Columns.jsx
+++ frontend/src/components/attendance/Columns.jsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { Ban, CheckCircle } from "lucide-react";
+import { Ban, CheckCircle, LockKeyhole, LockKeyholeOpen } from "lucide-react";
 
 // Define the columns for the table
 export const columns = [
@@ -31,58 +31,28 @@ export const columns = [
   },
   {
     accessorKey: "description",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Description
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: "Description",
     cell: ({ row }) => <div className="">{row.getValue("description")}</div>,
   },
   {
     accessorKey: "location",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Location
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: "Location",
     cell: ({ row }) => <div className="">{row.getValue("location")}</div>,
   },
   {
     accessorKey: "active",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Status
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: "Status",
     cell: ({ row }) => {
       const active = row.getValue("active");
       return (
         <div className="">
           {active ? (
-            <div className="flex items-center justify-center">
-              <CheckCircle className="size-4 mr-2 text-green-500" /> Open
+            <div className="flex items-center">
+              <LockKeyholeOpen className="size-4 mr-2 text-green-500" /> Open
             </div>
           ) : (
-            <div className="flex items-center justify-center">
-              <Ban className="size-4 mr-2 text-red-500" /> Close
+            <div className="flex items-center">
+              <LockKeyhole className="size-4 mr-2 text-red-500" /> Close
             </div>
           )}
         </div>
@@ -91,32 +61,12 @@ export const columns = [
   },
   {
     accessorKey: "start",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Start
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: "Start",
     cell: ({ row }) => <div className="">{row.getValue("start")}</div>,
   },
   {
     accessorKey: "end",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          End
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: "End",
     cell: ({ row }) => <div className="">{row.getValue("end")}</div>,
   },
 ];
--- frontend/src/pages/AttendancePage.jsx
+++ frontend/src/pages/AttendancePage.jsx
@@ -1,57 +1,49 @@
 import AttendanceTable from '@/components/attendance/AttendanceTable';
-import React from 'react'
-const data = [
-    {
-      title: "Team Meeting",
-      start: "2024-10-14 09:00",
-      end: "2024-10-14 10:30",
-      active: true,
-      location: "Conference Room A",
-      description: "Monthly team meeting to discuss project progress and roadblocks."
-    },
-    {
-      title: "Product Launch",
-      start: "2024-10-15 13:00",
-      end: "2024-10-15 14:30",
-      active: true,
-      location: "Main Hall",
-      description: "Official product launch event for the new line of devices."
-    },
-    {
-      title: "Marketing Strategy Review",
-      start: "2024-10-16 11:00",
-      end: "2024-10-16 12:00",
-      active: false,
-      location: "Online - Zoom",
-      description: "A review of the marketing strategy for Q4 campaigns."
-    },
-    {
-      title: "Client Feedback Session",
-      start: "2024-10-17 15:00",
-      end: "2024-10-17 16:30",
-      active: false,
-      location: "Conference Room B",
-      description: "Gathering client feedback for the latest product release."
-    },
-    {
-      title: "Team Building Workshop",
-      start: "2024-10-20 09:30",
-      end: "2024-10-20 17:00",
-      active: false,
-      location: "Outdoor Park",
-      description: "Full-day team-building activities to foster collaboration and teamwork."
-    },
-    {
-      title: "Board Meeting",
-      start: "2024-10-21 14:00",
-      end: "2024-10-21 17:00",
-      active: true,
-      location: "Executive Meeting Room",
-      description: "Quarterly board meeting to review company performance and future strategies."
-    },
-  ];
+import React, {useEffect, useState} from 'react'
+import apiClient from '@/api/axios';
+import { formatSimpleDateTime, formatDate } from '@/util/helpers';
   
 const AttendancePage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] =useState(false);
+
+  useEffect(() => {
+    fetchPayments();
+  }, []);
+
+  const fetchPayments = async () => {
+    const user = JSON.parse(localStorage.getItem("userData"));
+    try {
+      const { data } = await apiClient.get("/admin/event", {
+        headers: {
+          Authorization: user.token,
+        },
+      });
+
+      if (!data.success) {
+        setData([]);
+      } else {
+        const tableData = data.data.map((data) => (
+          {
+          id: data._id,
+          title: data.title,
+          description: data.description,
+          location:data.location,
+          start: formatSimpleDateTime(data.startDate),
+          end: formatSimpleDateTime(data.endDate),
+          active:data.active
+        }));
+        setData(tableData);
+      }
+      console.log(data)
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error.response.message);
+      setLoading(false);
+    }
+  };
+  
   return (
     <div className="bg-[#fefefe] shadow-lg rounded-lg border border-gray-200 text-gray-900 px-6 py-5 flex flex-col relative">
       <h1 className="font-bold">Event Attendance Table</h1>
