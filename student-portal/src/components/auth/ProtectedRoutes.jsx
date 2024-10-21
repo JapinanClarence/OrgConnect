feef12fde62c6eb597fb7d65d88e57052ee3579d
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-21 22:45:21 +0800

    implemented authentication

---
+++ student-portal/src/components/auth/ProtectedRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  // Check if the authentication status is still loading
+  if (isLoading) {
+    return <div className="h-screen my-auto mx-auto"></div>;
+  }
+
+  // Render the child routes if authenticated, otherwise redirect to login
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
