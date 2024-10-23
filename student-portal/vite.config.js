65b0ce25cd3f71cd57ae16c355674a558788d0fc
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-23 12:53:38 +0800

    updated welcomepage

--- student-portal/vite.config.js
+++ student-portal/vite.config.js
@@ -3,9 +3,50 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port: 3000
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touc-icon.png",],
+      manifest: {
+        name: "OrgConnect",
+        short_name:"OrgConnect",
+        description:"OrgConnect web application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-72x72.png",
+            sizes: "75x72",
+            type: "image/png",
+          },
+          {
+            src: "pwa-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "pwa-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        display: "standalone"
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
   },
   resolve: {
     alias: {
