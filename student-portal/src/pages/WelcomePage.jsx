68a4ed4479661bb78cebb0463c975c0124808eea
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-23 11:37:37 +0800

    implemented pwa

---
+++ student-portal/src/pages/WelcomePage.jsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Download } from "lucide-react";
+const WelcomePage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen flex flex-col p-5">
+      <div className="absolute left-0 pt-4 px-5 top-0 right-0 flex ">
+        <img
+          src="NavLogo lightmode.svg"
+          alt="OrgConnect logo"
+          className="w-36 ml-0 mr-auto"
+        />
+                <div>
+          <Button variant="outline" className="h-9">
+              Login
+          </Button>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col justify-center text-center gap-2">
+        <div>
+          <h1 className="text-2xl font-bold">Welcome to</h1>
+          <h2 className="text-4xl h-12 font-bold font-accent bg-clip-text text-transparent bg-gradient-to-r  from-orange-400 to-yellow-500">
+            OrgConnect
+          </h2>
+        </div>
+        <div className="">
+          <Button variant="outline" className="mx-auto">
+            <Download /> Install OrgConnect
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
--- student-portal/vite.config.js
+++ student-portal/vite.config.js
@@ -1,15 +1,57 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port: 3000
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "OrgConnect",
+        short_name:"OrgConnect",
+        description:"OrgConnect web application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-72x72.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-128x128.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-144x144.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        display: "standalone"
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
