27a13805e4b770178976e88cb99583510c9551b0
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-25 17:03:03 +0800

    automated db connecton

--- backend/server.js
+++ backend/server.js
@@ -8,38 +8,40 @@ import userRoutes from "./routes/user.js";
 import adminOrganizationRoutes from "./routes/admin/organization.js";
 import organizationRoutes from "./routes/organization.js";
 import adminEventRoutes from "./routes/admin/events.js";
-import attendanceRoutes from "./routes/admin/attendance.js"
+import attendanceRoutes from "./routes/admin/attendance.js";
 import adminAnnouncementRoutes from "./routes/admin/announcement.js";
 import adminPaymentRoutes from "./routes/admin/payment.js";
 import adminMembersRoutes from "./routes/admin/members.js";
-import adminOfficerRoutes from "./routes/admin/officer.js"
-import dashbordRoute from "./routes/admin/dashboard.js"
+import adminOfficerRoutes from "./routes/admin/officer.js";
+import dashbordRoute from "./routes/admin/dashboard.js";
 import eventRoutes from "./routes/events.js";
 import announcementRoutes from "./routes/announcements.js";
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD)
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD);
 const app = express();
 
 const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
 
-//connect to db
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("DB connected successfully");
-  })
-  .catch((err) => {
-    console.error("DB connection error:", err);
-  });
-//connect to local db
-// mongoose
-//   .connect(process.env.DATABASE_LOCAL)
-//   .then(() => {
-//     console.log("DB connected successfully");
-//   })
-//   .catch((err) => {
-//     console.error("DB connection error:", err);
-//   });
+if (NODE_ENV === "development") {
+  mongoose
+    .connect(process.env.DATABASE_LOCAL)
+    .then(() => {
+      console.log("Local DB connected successfully");
+    })
+    .catch((err) => {
+      console.error("DB connection error:", err);
+    });
+} else if (NODE_ENV === "production") {
+  mongoose
+    .connect(DB)
+    .then(() => {
+      console.log("DB connected successfully");
+    })
+    .catch((err) => {
+      console.error("DB connection error:", err);
+    });
+}
 
 app.use(cors());
 
@@ -49,24 +51,23 @@ app.use("/api/", auth);
 app.use("/api/admin/", adminRoutes);
 
 //admin routes
-app.use("/api/superadmin/", superAdminRoutes)
-app.use("/api/admin/", adminOrganizationRoutes)
-app.use("/api/admin/", adminEventRoutes)
-app.use("/api/admin/", adminAnnouncementRoutes)
-app.use("/api/admin/", adminPaymentRoutes)
+app.use("/api/superadmin/", superAdminRoutes);
+app.use("/api/admin/", adminOrganizationRoutes);
+app.use("/api/admin/", adminEventRoutes);
+app.use("/api/admin/", adminAnnouncementRoutes);
+app.use("/api/admin/", adminPaymentRoutes);
 app.use("/api/admin/", adminMembersRoutes);
 app.use("/api/admin/", adminOfficerRoutes);
 app.use("/api/admin", dashbordRoute);
 //attendance routes
-app.use("/api/attendance", attendanceRoutes)
+app.use("/api/attendance", attendanceRoutes);
 //user routes
 app.use("/api/user/", userRoutes);
 app.use("/api/user/", organizationRoutes);
-app.use("/api/user/", eventRoutes)
-app.use("/api/user/", announcementRoutes)
+app.use("/api/user/", eventRoutes);
+app.use("/api/user/", announcementRoutes);
 // Handle 404 errors for undefined routes
 app.use((req, res, next) => {
- 
   res.status(404).json({
     success: false,
     message: "Page not found",
