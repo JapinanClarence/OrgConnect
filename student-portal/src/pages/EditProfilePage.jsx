96226b2fe525e4aaf488a2d3fb579c919b49fb63
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-25 18:07:04 +0800

    added loading state on edit page

--- student-portal/src/pages/EditProfilePage.jsx
+++ student-portal/src/pages/EditProfilePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import EditProfileForm from "@/components/profile/EditProfileForm";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, LoaderCircle } from "lucide-react";
 import apiClient from "@/api/axios";
 import { useAuth } from "@/context/AuthContext";
 import { formatToDateInput, formatDate } from "@/util/helpers";
@@ -78,7 +78,7 @@ const EditProfilePage = () => {
   };
 
   return (
-    <div className="mt-[2.8rem] py-5 px-5">
+    <div className="pt-[2.8rem] h-screen p-5">
       <header className="fixed top-0 left-0 w-full z-10 ">
         <div className="px-3 py-2   bg-slate-50 shadow-sm border-b  flex items-center justify-between">
           <div className="h-7 inline-flex justify-center items-center">
@@ -88,13 +88,21 @@ const EditProfilePage = () => {
           </div>
         </div>
       </header>
-      {userData && (
-        <EditProfileForm
-          userData={userData}
-          onSubmit={onSubmit}
-          errorMessage={errorMessage}
-          isSubmitting={isSubmitting}
-        />
+      {loading ? (
+        <div className="h-full flex justify-center items-center">
+          <LoaderCircle size={20} className="animate-spin "/>
+        </div>
+      ) : (
+        userData && (
+          <div className="mt-5">
+          <EditProfileForm
+            userData={userData}
+            onSubmit={onSubmit}
+            errorMessage={errorMessage}
+            isSubmitting={isSubmitting}
+          />
+          </div>
+        )
       )}
     </div>
   );
