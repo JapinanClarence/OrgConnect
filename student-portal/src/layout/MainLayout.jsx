c82b2c6e11454b96e3445a51e5021ad0442b5d36
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-10-31 18:14:15 +0800

    display the overlay only on large screens

--- student-portal/src/layout/MainLayout.jsx
+++ student-portal/src/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <div className="h-screen bg-slate-50 relative">
-      <div className="hidden md:absolute h-screen bg-slate-50 left-0 right-0 z-50 md:flex justify-center items-center">
+      <div className="hidden absolute h-screen bg-slate-50 left-0 right-0 z-50 lg:flex justify-center items-center">
         <h1 className="text-muted-foreground">Application is only available on mobile.</h1>
       </div>
       <Outlet />
