a733c6255c799173fc2b416306f170d9abfbd821
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-02-24 20:51:36 +0800

    testing new date format

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -218,8 +218,8 @@ const EventPage = () => {
             return {
               id: event._id,
               title: event.title,
-              startDate: formatSimpleDateTime(event.startDate),
-              endDate: formatSimpleDateTime(event.endDate),
+              startDate: formatDate(event.startDate),
+              endDate: formatDate(event.endDate),
               description: event.description,
               status: event.status,
               location: event.location,
