347131d6637f871c447f2a2ea97d28edf444cdb1
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-02-24 08:28:53 +0800

    added locale time zone

--- frontend/src/util/helpers.js
+++ frontend/src/util/helpers.js
@@ -2,48 +2,52 @@
 export const formatDate = (dateString) => {
   const date = new Date(dateString); // Parse the date string into a Date object
   return date.toLocaleString("en-PH", {
-    weekday: "long",      // Sunday, Monday, etc.
-    year: "numeric",      // Full year
-    month: "long",        // Full month name
-    day: "2-digit",       // Day of the month with leading zero
-    hour: "numeric",      // Hour in 12-hour format
-    minute: "numeric",    // Minutes
-    hour12: true          // AM/PM format
+    weekday: "long", // Sunday, Monday, etc.
+    year: "numeric", // Full year
+    month: "long", // Full month name
+    day: "2-digit", // Day of the month with leading zero
+    hour: "numeric", // Hour in 12-hour format
+    minute: "numeric", // Minutes
+    hour12: true, // AM/PM format
+    timeZone: "Asia/Manila",
   });
 };
 
 export const formatSimpleDateTime = (dateString) => {
   const date = new Date(dateString); // Parse the date string into a Date object
   return date.toLocaleString("en-PH", {
-    year: "numeric",      // Full year
-    month: "2-digit",     // 2-digit month (MM)
-    day: "2-digit",       // Day of the month with leading zero (DD)
-    hour: "numeric",      // Hour in 12-hour format
-    minute: "numeric",    // Minutes
-    hour12: true          // AM/PM format
+    year: "numeric", // Full year
+    month: "2-digit", // 2-digit month (MM)
+    day: "2-digit", // Day of the month with leading zero (DD)
+    hour: "numeric", // Hour in 12-hour format
+    minute: "numeric", // Minutes
+    hour12: true, // AM/PM format
+    timeZone: "Asia/Manila",
   });
 };
 
-export const formatSimpleDate = (dateString) =>{
+export const formatSimpleDate = (dateString) => {
   const date = new Date(dateString); // Parse the date string into a Date object
   return date.toLocaleString("en-PH", {
-    year: "numeric",      // Full year
-    month: "long",     // full month
-    day: "2-digit",       // Day of the month with leading zero (DD)
+    year: "numeric", // Full year
+    month: "long", // full month
+    day: "2-digit", // Day of the month with leading zero (DD)
+    timeZone: "Asia/Manila",
   });
-}
-export const dateOnly = (dateString) =>{
+};
+export const dateOnly = (dateString) => {
   const date = new Date(dateString); // Parse the date string into a Date object
   return date.toLocaleString("en-PH", {
-    year: "numeric",      // Full year
-    month: "2-digit",     // 2-digit month (MM)
-    day: "2-digit",       // Day of the month with leading zero (DD)
+    year: "numeric", // Full year
+    month: "2-digit", // 2-digit month (MM)
+    day: "2-digit", // Day of the month with leading zero (DD)
+    timeZone: "Asia/Manila",
   });
-}
+};
 export const dateOnlyISO = (dateString) => {
-  const date = new Date(dateString); // Parse the date string into a Date object
+  const date = new Date(dateString).toLocaleString("en-PH", {timeZone:"Asia/Manila"}); // Parse the date string into a Date object
   const year = date.getFullYear(); // Get the full year
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get the month and pad with leading zero
-  const day = date.getDate().toString().padStart(2, '0'); // Get the day and pad with leading zero
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Get the month and pad with leading zero
+  const day = date.getDate().toString().padStart(2, "0"); // Get the day and pad with leading zero
   return `${year}-${month}-${day}`;
 };
